4b3d92485f3235923498dcc214a6136b4514d067
Qaswar-Hasan <kaswarhasan898@gmail.com>
2026-08-07 20:39:15 +0300

    build: bump version to 2.6 and update gradle action

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.factory.mouthpiece"
         minSdk = 24
         targetSdk = 34
-        versionCode = 25
-        versionName = "2.5"
+        versionCode = 26
+        versionName = "2.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
